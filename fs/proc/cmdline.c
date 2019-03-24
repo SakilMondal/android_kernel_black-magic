79fed5ea7625cffdea9765d2f4a7171e92a56aae
YaAlex <yaalex113@gmail.com>
2019-03-24 11:39:30 +0200

    ZC550KL:Fix offline charging
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -4,11 +4,11 @@
 #include <linux/seq_file.h>
 #include <asm/setup.h>
 
-static char proc_cmdline[COMMAND_LINE_SIZE];
+static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", proc_cmdline);
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
@@ -24,23 +24,58 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+static void remove_flag(char *cmd, const char *flag)
+{
+	char *start_addr, *end_addr;
+	
+	/* Ensure all instances of a flag are removed */
+	while ((start_addr = strstr(cmd, flag))) {
+		end_addr = strchr(start_addr, ' ');
+		if (end_addr)
+			memmove(start_addr, end_addr + 1, strlen(end_addr));
+		else
+			*(start_addr - 1) = '\0';
+	}
+}
+
+static void remove_safetynet_flags(char *cmd)
+{
+	remove_flag(cmd, "androidboot.enable_dm_verity=");
+	remove_flag(cmd, "androidboot.secboot=");
+	remove_flag(cmd, "androidboot.verifiedbootstate=");
+	remove_flag(cmd, "androidboot.veritymode=");
+}
+
 static int __init proc_cmdline_init(void)
 {
-	/* SafetyNet bypass: show androidboot.verifiedbootstate=green */
-	char *a1, *a2;
+	strcpy(new_command_line, saved_command_line);
 
-	a1 = strstr(saved_command_line, "androidboot.verifiedbootstate=");
-	if (a1) {
-		a1 = strchr(a1, '=');
-		a2 = strchr(a1, ' ');
-		if (!a2) /* last argument on the cmdline */
-			a2 = "";
+	/*
+	 * Remove various flags from command line seen by userspace in order to
+	 * pass SafetyNet CTS check.
+	 */
+	remove_safetynet_flags(new_command_line);
 
-		scnprintf(proc_cmdline, COMMAND_LINE_SIZE, "%.*sgreen%s",
-			  (int)(a1 - saved_command_line + 1),
-			  saved_command_line, a2);
+	char *offset_addr, *cmd = new_command_line;
+
+	strcpy(cmd, saved_command_line);
+
+	offset_addr = strstr(cmd, "androidboot.mode=charger_shipping");
+	if (offset_addr) {
+		size_t i, len, offset;
+		len = strlen(cmd);
+		offset = offset_addr - cmd;
+		for (i = 1; i < (len - offset); i++) {
+			if (cmd[offset + i] == ' ')
+				break;
+		}
+		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
+
+		strcat(cmd, " androidboot.mode=charger");
 	} else {
-		strncpy(proc_cmdline, saved_command_line, COMMAND_LINE_SIZE);
+		if (g_Charger_mode == true || g_ASUS_bootmode == CHARGER_SHIPPING_MODE) {
+			strcat(cmd, " androidboot.mode=charger");
+		}
 	}
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
